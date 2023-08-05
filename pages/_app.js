cc167ed4c4d2fb3fac6f8250a39908ca3833b0b0
Mohammad Hosein <en.mohammadhosein@gmail.com>
2023-08-06 00:34:21 +0330

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,13 @@
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+      <>
+        <h1>Header Section</h1>
+        <hr/>
+        <Component {...pageProps} />
+        <hr/>
+        <h1>Footer Section</h1>
+      </>
+  )
 }
