d947010724be830b9d4d80c96320cdfe4e496ed3
Mohammad Hosein <en.mohammadhosein@gmail.com>
2023-08-06 00:23:04 +0330

    feat: Pages 404 , 500

---
+++ pages/404.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function NotFound()
+{
+    return (
+        <h1>Error 404  -  Page Not Found!!!</h1>
+    )
+}
---
+++ pages/500.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export default function Error500()
+{
+    return (
+        <h1>Error 50: Server Side Error!</h1>
+    )
+}
