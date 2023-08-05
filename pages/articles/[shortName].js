f0399176bf1ff67c79405809ec687ec58f6dcc47
Mohammad Hosein <en.mohammadhosein@gmail.com>
2023-08-05 19:48:58 +0330

    feat: useRoter

--- pages/articles/[shortName].js
+++ pages/articles/[shortName].js
@@ -1,7 +1,14 @@
+import { useRouter } from "next/router";
 import React from "react";
 
 function ArticleDetails() {
-  return <h1>Article Details</h1>;
+  const router = useRouter()
+  // console.log(router.query);
+  return (
+    <>
+      <h1>Article Details: ({router.query.shortName})</h1>
+    </>
+  );
 }
 
 export default ArticleDetails;
